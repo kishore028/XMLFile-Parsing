cdd23b9924958d498f37490891fb5430bac6219c
kishore028 <151540295+kishore028@users.noreply.github.com>
2025-09-22 17:57:55 +0530

    Add files via upload

---
+++ thym/thym/src/test/java/com/example/thym/ThymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
