cc05ee00a94c52ba8de1f64c323cd145e301a95b
Kishore <151540295+kishore028@users.noreply.github.com>
2025-10-28 12:50:25 +0530

    Add files via upload
    todo list

---
+++ fordev/fordev/src/test/java/com/example/fordev/FordevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fordev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FordevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
